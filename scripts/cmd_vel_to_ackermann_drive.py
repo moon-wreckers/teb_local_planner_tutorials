2586a3dabe8ff54249a432d9ee66c7624b274c00
Daniel Arnett <arnett.daniel@gmail.com>
2018-03-02 09:28:39 -0500

    Driving krawler

--- scripts/cmd_vel_to_ackermann_drive.py
+++ scripts/cmd_vel_to_ackermann_drive.py
@@ -20,39 +20,38 @@ def cmd_callback(data):
   global ackermann_cmd_topic
   global frame_id
   global pub
-  
+
   v = data.linear.x
   steering = convert_trans_rot_vel_to_steering_angle(v, data.angular.z, wheelbase)
-  
+
   msg = AckermannDriveStamped()
   msg.header.stamp = rospy.Time.now()
   msg.header.frame_id = frame_id
   msg.drive.steering_angle = steering
   msg.drive.speed = v
-  
+
   pub.publish(msg)
-  
 
 
 
 
-if __name__ == '__main__': 
+
+if __name__ == '__main__':
   try:
-    
+
     rospy.init_node('cmd_vel_to_ackermann_drive')
-        
-    twist_cmd_topic = rospy.get_param('~twist_cmd_topic', '/cmd_vel') 
+
+    twist_cmd_topic = rospy.get_param('~twist_cmd_topic', '/ak1/cmd_vel') 
     ackermann_cmd_topic = rospy.get_param('~ackermann_cmd_topic', '/ackermann_cmd')
     wheelbase = rospy.get_param('~wheelbase', 1.0)
     frame_id = rospy.get_param('~frame_id', 'odom')
-    
+
     rospy.Subscriber(twist_cmd_topic, Twist, cmd_callback, queue_size=1)
     pub = rospy.Publisher(ackermann_cmd_topic, AckermannDriveStamped, queue_size=1)
-    
+
     rospy.loginfo("Node 'cmd_vel_to_ackermann_drive' started.\nListening to %s, publishing to %s. Frame id: %s, wheelbase: %f", "/cmd_vel", ackermann_cmd_topic, frame_id, wheelbase)
-    
+
     rospy.spin()
-    
+
   except rospy.ROSInterruptException:
     pass
-
